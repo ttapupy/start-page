46db19f5450832efa053f1130c0b401efe685f00
krisz <kkurtisz@gmail.com>
2024-04-15 19:57:42 +0200

    Revert "mobil focus fix"
    This reverts commit 472ef627f432cafdd4e5ceb4c0bed60b6425788e.

--- app/components/FeedCard.tsx
+++ app/components/FeedCard.tsx
@@ -89,6 +89,7 @@ const FeedCard: React.FC<IFeedCardProps> = ({
 
   React.useEffect(() => {
     if (hidden) {
+
       delay.current = window.setTimeout(() => handleHide(guidOrLink, sourceKey, expDays), 800)
     }
 
